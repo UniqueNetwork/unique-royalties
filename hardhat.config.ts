8800533c3bb9525ce53921c606070666684fc29a
ashkuc <ashkucc@gmail.com>
2023-03-22 14:47:44 +0100

    remove commented part from config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,18 +4,6 @@ import "@nomicfoundation/hardhat-toolbox";
 const config: HardhatUserConfig = {
   solidity: {
     version: "0.8.18",
-    // settings: {
-    //   metadata: {
-    //     // Not including the metadata hash
-    //     // https://github.com/paulrberg/solidity-template/issues/31
-    //     bytecodeHash: "none",
-    //   },
-    //   optimizer: {
-    //     enabled: true,
-    //     runs: 800,
-    //   },
-    //   viaIR : true,
-    // },
   },
   networks: {
     hardhat: {},
