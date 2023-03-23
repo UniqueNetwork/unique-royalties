1505385bc692bc893a161cc143324f51df93c51d
ashkuc <ashkucc@gmail.com>
2023-03-23 14:12:16 +0100

    remove unused tests

--- test/GasPrices.ts
+++ test/GasPrices.ts
@@ -5,9 +5,7 @@ import { Ethereum } from '@unique-nft/utils/extension';
 import { HexString } from '@unique-nft/utils/string';
 import { uniqueRoyaltyTypes } from "./_util";
 
-describe('Royalties', () => {
-    const equalsIgnoreCase = (a?: string, b?: string) => expect(a?.toLowerCase()).to.equal(b?.toLowerCase());
-
+describe('Gas prices', () => {
     async function deployFixture() {
         const [owner] = await ethers.getSigners();
 
@@ -18,13 +16,20 @@ describe('Royalties', () => {
         return { gasPricesContract, owner };
     }
 
+    const logDiff = (name: string, {strReceipt, abiReceipt}: { strReceipt: any, abiReceipt: any }) => {
+        console.dir({
+            name,
+            strReceipt: strReceipt.gasUsed,
+            abiReceipt: abiReceipt.gasUsed,
+            diff: (strReceipt.gasUsed / abiReceipt.gasUsed),
+        });
+    }
+
     it('Compare CrossAddress', async () => {
         const { gasPricesContract } = await loadFixture(deployFixture);
 
         const encoded = {
-            abi: '0x' +
-                '0000000000000000000000001234a38988dd5ecc93dd9ce90a44a00e5fb91e4c' +
-                '0000000000000000000000000000000000000000000000000000000000000000',
+            abi: '0x0000000000000000000000001234a38988dd5ecc93dd9ce90a44a00e5fb91e4c0000000000000000000000000000000000000000000000000000000000000000',
             str: '0x1234a38988dd5ecc93dd9ce90a44a00e5fb91e4c',
         };
 
@@ -36,21 +41,14 @@ describe('Royalties', () => {
 
         expect(abiReceipt.events.CrossAddressDecoded).to.deep.equal(strReceipt.events.CrossAddressDecoded);
 
-        console.dir({
-            strReceipt: strReceipt.gasUsed,
-            abiReceipt: abiReceipt.gasUsed,
-            diff: strReceipt.gasUsed - abiReceipt.gasUsed,
-        });
+        logDiff('CrossAddress', {strReceipt, abiReceipt});
     });
 
     it('Compare UniqueRoyaltyPart', async () => {
         const { gasPricesContract } = await loadFixture(deployFixture);
 
         const encoded = {
-            abi: '0x' +
-                + '0000000000000000000000001234a38988dd5ecc93dd9ce90a44a00e5fb91e4c'
-                + '0000000000000000000000000000000000000000000000000000000000000000'
-                + '0000000000000000000000000000000000000000000000000000000000030d40',
+            abi: '0x0000000000000000000000001234a38988dd5ecc93dd9ce90a44a00e5fb91e4c00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000030d40',
             str: 'e-1234A38988Dd5ecC93Dd9cE90a44A00e5FB91e4C-0200000',
         };
 
@@ -63,48 +61,28 @@ describe('Royalties', () => {
 
         expect(abiReceipt.events.UniqueRoyaltyPartDecoded).to.deep.equal(strReceipt.events.UniqueRoyaltyPartDecoded);
 
-        console.dir({
-            strReceipt: strReceipt.gasUsed,
-            abiReceipt: abiReceipt.gasUsed,
-            diff: strReceipt.gasUsed - abiReceipt.gasUsed,
-        });
+        logDiff('UniqueRoyaltyPart', {strReceipt, abiReceipt});
     });
 
-    // it('Compare UniqueRoyalty', async () => {
-    //     const { gasPricesContract } = await loadFixture(deployFixture);
-    //
-    //     const encoded = {
-    //         abi: '0x' +
-    //             + '0000000000000000000000000000000000000000000000000000000000000001'
-    //             + '0000000000000000000000000000000000000000000000000000000000000006'
-    //             + '0000000000000000000000000000000000000000000000000000000000000080'
-    //             + '0000000000000000000000000000000000000000000000000000000000000160'
-    //             + '0000000000000000000000000000000000000000000000000000000000000002'
-    //             + '0000000000000000000000001234a38988dd5ecc93dd9ce90a44a00e5fb91e4c'
-    //             + '0000000000000000000000000000000000000000000000000000000000000000'
-    //             + '0000000000000000000000000000000000000000000000000000000000030d40'
-    //             + '0000000000000000000000000000000000000000000000000000000000000000'
-    //             + 'd43593c715fdd31c61141abd04a99fd6822c8558854ccde39a5684e7a56da27d'
-    //             + '000000000000000000000000000000000000000000000000000000000000186a'
-    //             + '0000000000000000000000000000000000000000000000000000000000000000',
-    //         str: `d:06|v:0001|`
-    //             + `P-0125:e-1234A38988Dd5ecC93Dd9cE90a44A00e5FB91e4C-0200000;s-d43593c715fdd31c61141abd04a99fd6822c8558854ccde39a5684e7a56da27d-0006250` + ';'
-    //             + `S-0125:s-d43593c715fdd31c61141abd04a99fd6822c8558854ccde39a5684e7a56da27d-0006250;e-1234A38988Dd5ecC93Dd9cE90a44A00e5FB91e4C-0200000`,
-    //     };
-    //
-    //
-    //     const abiTx = await gasPricesContract.uniqueRoyaltyFromAbiEncoded(HexString.toU8a(encoded.abi));
-    //     const abiReceipt = Ethereum.parseEthersTxReceipt(await abiTx.wait());
-    //
-    //     const strTx = await gasPricesContract.uniqueRoyaltyFromString(encoded.str);
-    //     const strReceipt = Ethereum.parseEthersTxReceipt(await strTx.wait());
-    //
-    //     expect(abiReceipt.events.UniqueRoyaltyDecoded).to.deep.equal(strReceipt.events.UniqueRoyaltyDecoded);
-    //
-    //     console.dir({
-    //         strReceipt: strReceipt.gasUsed,
-    //         abiReceipt: abiReceipt.gasUsed,
-    //         diff: strReceipt.gasUsed - abiReceipt.gasUsed,
-    //     });
-    // });
+    it('Compare UniqueRoyalty', async () => {
+        const { gasPricesContract } = await loadFixture(deployFixture);
+
+        const encoded = {
+            abi: '0x0000000000000000000000000000000000000000000000000000000000000020000000000000000000000000000000000000000000000000000000000000000100000000000000000000000000000000000000000000000000000000000000060000000000000000000000000000000000000000000000000000000000000080000000000000000000000000000000000000000000000000000000000000016000000000000000000000000000000000000000000000000000000000000000020000000000000000000000001234a38988dd5ecc93dd9ce90a44a00e5fb91e4c00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000030d400000000000000000000000000000000000000000000000000000000000000000d43593c715fdd31c61141abd04a99fd6822c8558854ccde39a5684e7a56da27d000000000000000000000000000000000000000000000000000000000000186a00000000000000000000000000000000000000000000000000000000000000020000000000000000000000000000000000000000000000000000000000000000d43593c715fdd31c61141abd04a99fd6822c8558854ccde39a5684e7a56da27d000000000000000000000000000000000000000000000000000000000000186a0000000000000000000000001234a38988dd5ecc93dd9ce90a44a00e5fb91e4c00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000030d40',
+            str: `d:06|v:0001|`
+                + `P-0125:e-1234A38988Dd5ecC93Dd9cE90a44A00e5FB91e4C-0200000;s-d43593c715fdd31c61141abd04a99fd6822c8558854ccde39a5684e7a56da27d-0006250` + ';'
+                + `S-0125:s-d43593c715fdd31c61141abd04a99fd6822c8558854ccde39a5684e7a56da27d-0006250;e-1234A38988Dd5ecC93Dd9cE90a44A00e5FB91e4C-0200000`,
+        };
+
+
+        const abiTx = await gasPricesContract.uniqueRoyaltyFromAbiEncoded(HexString.toU8a(encoded.abi));
+        const abiReceipt = Ethereum.parseEthersTxReceipt(await abiTx.wait());
+
+        const strTx = await gasPricesContract.uniqueRoyaltyFromString(encoded.str);
+        const strReceipt = Ethereum.parseEthersTxReceipt(await strTx.wait());
+
+        expect(abiReceipt.events.UniqueRoyaltyDecoded).to.deep.equal(strReceipt.events.UniqueRoyaltyDecoded);
+
+        logDiff('UniqueRoyalty', {strReceipt, abiReceipt});
+    });
 });
